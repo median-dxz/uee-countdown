c12ebe90f5a3731500fa86dfcb5d08ec840ec4f0
median-dxz <med_iridescence@163.com>
2019-08-06 18:39:28 +0800

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,42 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: "babel-eslint",
-    ecmaVersion: 2017,
-    sourceType: "module"
+      "parser": "babel-eslint",
+      "ecmaVersion": 2017,
+      "sourceType": "module"
   },
   env: {
     browser: true,
     node: true,
     es6: true
   },
-
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-
-  plugins: ["vue", "prettier"],
+  
+  extends: [
+    'plugin:vue/essential',
+     "@vue/prettier"
+  ],
+  
+  plugins: [
+    'vue',
+    'prettier'
+  ],
 
   globals: {
     __static: true
   },
 
-  rules: {
+  'rules': {
     // allow paren-less arrow functions
-    "arrow-parens": 0,
+    'arrow-parens': 0,
     // allow async-await
-    "generator-star-spacing": 0,
+    'generator-star-spacing': 0,
 
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
 
-    "no-extra-parens": 0,
-    "no-multi-str": 0,
-    "no-multiple-empty-lines": [0, { max: 3 }]
-  }
-};
+    'no-extra-parens': 0,
+    'no-multi-str': 0,
+    'no-multiple-empty-lines': [0, {'max': 3}]
+  },
+  
+}
